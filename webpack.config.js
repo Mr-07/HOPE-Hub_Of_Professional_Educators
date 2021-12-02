9cd1b6af6b17a30274cdef20f42d1ebb1bfa07db
atulbansal4444 <atulbansal4444@gmail.com>
2021-12-02 22:29:57 +0530

    webpack reconfigured

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,45 @@
-import { resolve as _resolve } from "path";
-import webpack from "webpack";
-import HTMLWebpackPlugin from "html-webpack-plugin";
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-export const entry = "./src/index.js";
-export const mode = "development";
-export const output = {
-    path: _resolve("build"),
-};
-export const module = {
-    rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: "babel-loader",
-        },
-        {
-            test: /\.html$/,
-            use: "html-loader",
-        },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: "file-loader",
-        },
-        {
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
-        },
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        path: path.resolve("build"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: "file-loader",
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "index.html",
+            filename: "index.html"
+        }),
     ],
-};
-export const plugins = [
-    new HTMLWebpackPlugin({
-        template: "index.html",
-        filename: "index.html"
-    }),
-];
-export const resolve = {
-    alias: {
-        'components': _resolve(__dirname, "src/components/"),
-        'images': _resolve(__dirname, "src/images/"),
-        'common': _resolve(__dirname, "src/common/"),
+    resolve: {
+        alias: {
+            'components': path.resolve(__dirname, "src/components/"),
+            'images': path.resolve(__dirname, "src/images/"),
+            'common': path.resolve(__dirname, "src/common/"),
+        },
     },
 };
