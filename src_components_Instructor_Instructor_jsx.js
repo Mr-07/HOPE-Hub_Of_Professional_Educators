21ee9b8196688b8c074759426740c3af74f19335
atulbansal4444 <atulbansal4444@gmail.com>
2021-12-02 21:55:46 +0530

    Updates

---
+++ src_components_Instructor_Instructor_jsx.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkhope"] = self["webpackChunkhope"] || []).push([["src_components_Instructor_Instructor_jsx"],{
+
+/***/ "./src/components/Instructor/Instructor.jsx":
+/*!**************************************************!*\
+  !*** ./src/components/Instructor/Instructor.jsx ***!
+  \**************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Instructor)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nfunction Instructor(props) {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    id: `instructor-content`,\n    style: {\n      height: 'calc(100vh)',\n      backgroundColor: 'green'\n    }\n  }));\n}\n\n//# sourceURL=webpack://hope/./src/components/Instructor/Instructor.jsx?");
+
+/***/ })
+
+}]);
