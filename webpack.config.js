3ed8c790d219792aa326a5cca7691b9235bf43ac
atulbansal4444 <atulbansal4444@gmail.com>
2021-12-02 22:23:30 +0530

    adding deploy command

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-const path = require("path");
-const webpack = require("webpack");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+import { resolve as _resolve } from "path";
+import webpack from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    output: {
-        path: path.resolve("dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: "babel-loader",
-            },
-            {
-                test: /\.html$/,
-                use: "html-loader",
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: "file-loader",
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: "index.html",
-            filename: "index.html"
-        }),
-    ],
-    resolve: {
-        alias: {
-            'components': path.resolve(__dirname, "src/components/"),
-            'images': path.resolve(__dirname, "src/images/"),
-            'common': path.resolve(__dirname, "src/common/"),
+export const entry = "./src/index.js";
+export const mode = "development";
+export const output = {
+    path: _resolve("build"),
+};
+export const module = {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: "babel-loader",
         },
+        {
+            test: /\.html$/,
+            use: "html-loader",
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: "file-loader",
+        },
+        {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+        },
+    ],
+};
+export const plugins = [
+    new HTMLWebpackPlugin({
+        template: "index.html",
+        filename: "index.html"
+    }),
+];
+export const resolve = {
+    alias: {
+        'components': _resolve(__dirname, "src/components/"),
+        'images': _resolve(__dirname, "src/images/"),
+        'common': _resolve(__dirname, "src/common/"),
     },
 };
